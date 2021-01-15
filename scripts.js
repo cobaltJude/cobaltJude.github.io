380081b958ec357d9d348a5f20b3065ba2cffca5
Jude Bigboy <75493010+cobaltJude@users.noreply.github.com>
2021-01-14 21:05:01 -0500

    updated with sayo's site

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    alert("WHY ARE YOU HERE");
+    $("#rom").draggable();
+        $("#walkthrububble").draggable();
+    $("#walkthrububble").hover(function(){
+        $("#walkthrough").show();
+    });
+    
+})
+
+
+
