348630f1520ad48d4c57cb54f1b8b37e88333166
Jude Bigboy <75493010+cobaltJude@users.noreply.github.com>
2021-01-24 22:54:28 -0500

    Reworked Purpose of Site
    Reworked the purpose of site, including
    - New title
    - Less memery
    - Calendar table for eventual monthly tag searching
    - Updated Header
    TO DO
    - Implement tag searching system
    - Implement ability to assign searchable tags to an <article> semantic block
    - Implement actual calendar links
    - Link to actual projects
    - Figure out RSS feed for podcast (A Much Unneeded Analysis)

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-    alert("WHY ARE YOU HERE");
-    $("#rom").draggable();
-        $("#walkthrububble").draggable();
-    $("#walkthrububble").hover(function(){
-        $("#walkthrough").show();
-    });
-    
 })
 
 
