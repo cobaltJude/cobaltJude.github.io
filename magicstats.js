7586d9e3e91709e6a0cc90d46765e39a0714de8d
Jude Bigboy <75493010+cobaltJude@users.noreply.github.com>
2021-01-29 20:08:01 -0500

    Update magicstats.js

--- magicstats.js
+++ magicstats.js
@@ -1,6 +1,3 @@
-jQuery.getJson("https://mtgjson.com/api/v5/Standard.json", "MagicStats", "Success!");
-
-$(document).ready(function() {
-    var mydata = JSON.parse(data);
-    alert(mydata[0].card)
-})
+$.getJson("Standard.json")
+var mydata = JSON.parse(data);
+window.alert(mydata[0].baseSetSize);
