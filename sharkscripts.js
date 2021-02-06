64ed141d671a267614a76344ec56921d83084f15
Jude Bigboy <75493010+cobaltJude@users.noreply.github.com>
2021-02-05 19:54:43 -0500

    SHARK TIME BABY

---
+++ sharkscripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#stars").hide();
+    $("#shark").click(function(){
+        $("#stars").show();
+        $("#sharktalk").text('Thank you! My teeth feel so clean now.');
+    })
+
+});
+
