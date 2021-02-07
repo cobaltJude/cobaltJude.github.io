3d4404e33ae5feaaf214a93e474d4f1a20c3c4fc
Jude Bigboy <75493010+cobaltJude@users.noreply.github.com>
2021-02-07 00:51:08 -0500

    JESUS BALLIN

---
+++ jesusscripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('#jesus').click(function(){ 
+        $('#jesus').fadeOut(1000);
+        $('h1').after("<a href='hotsingles'>click me ;)</a>");
+    });
+
+    function loop() {
+        $('#jesussays').animate({bottom:'100px'},750);
+        $('#jesussays').animate({left:'15%'},750);
+        $('#jesussays').animate({bottom:'500px'},750);
+        $('#jesussays').animate({left:'65%'},750, loop);
+    }
+
+   
+    loop();
+
+
+});
+
